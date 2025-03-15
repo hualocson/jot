9a3dd4e65e7d5df9894b3b6a2e3ff53003a43bde
locson <hualocson@gmail.com>
2025-03-15 18:27:54 +0700

    Add APP_ENV variable to environment configuration

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
+    APP_ENV: z.enum(["development", "production"]),
   },
   client: {},
   // For Next.js >= 13.4.4, you only need to destructure client variables:
