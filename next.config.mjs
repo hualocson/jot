f36f531eb99785e509e9520fba3410ed0bad0da4
locson <hualocson@gmail.com>
2025-03-15 18:27:28 +0700

    Add remote image patterns for Cloudinary in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const jiti = createJiti(fileURLToPath(import.meta.url));
 await jiti.import("./src/env");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
